59a666fd05975b7eada781771a2996c89f377348
Zeeshan Khan <zeeshankh1980@gmail.com>
2026-04-25 20:55:21 +0200

    upgrade UI to views + improve structure

--- app/index.js
+++ app/index.js
@@ -1,28 +1,11 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send(`
-    <style>
-      body { font-family: Arial; text-align: center; background: #f4f6f7; }
-      h1 { color: #2c3e50; }
-      button { padding: 10px; background: #3498db; color: white; border: none; }
-      input { padding: 8px; }
-    </style>
-
-    <h1>Zipp Logistics 🚚</h1>
-    <p>Fast & Reliable Delivery</p>
-
-    <h3>Track Shipment</h3>
-    <form method="POST" action="/track">
-      <input name="trackingId" placeholder="Enter Tracking ID" />
-      <button type="submit">Track</button>
-    </form>
-
-    <p><a href="/about">About Us</a></p>
-  `)
+ app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"))
 })
 
 app.post("/track", (req, res) => {
